2417ba5d4ea5ac3bff2ef7255c6fa8c9a90d5d53
Michal Fratczak <michal@cgarea.com>
2019-02-15 13:22:51 +0100

    Added example py_client/simple_listener.py

---
+++ code/pyClient/simple_listener.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+
+import threading
+import time
+from ws4py.client.threadedclient import WebSocketClient # pip install ws4py
+
+
+WS = None # websocket
+EXIT = False
+
+
+class HabDecClient(WebSocketClient):
+	def received_message(self, m):
+		print m.data
+	def opened(self):
+		print "Opened Connection"
+	def closed(self, code, reason=None):
+		print "Closed down", code, reason
+
+
+def RequestRttyStream():
+	global WS
+	while WS and not EXIT:
+		WS.send("cmd::liveprint")
+		# WS.send("*") # or just send anything to be notified with cmd::info:sentence
+		time.sleep(.25)
+
+
+def main(i_srv = 'ws://localhost:5555'):
+	global WS
+	WS = HabDecClient( i_srv )
+	WS.connect()
+
+	threading.Thread( target = lambda: WS.run_forever() ).start()
+	threading.Thread( target = RequestRttyStream ).start()
+
+	# exit on Ctrl-C
+	while 1:
+		try:
+			time.sleep(1)
+		except KeyboardInterrupt:
+			global EXIT
+			EXIT = True
+			WS.close()
+			return
+
+
+if __name__ == '__main__':
+	main()
