michalfratczak/habdec

commit 1488dafe4835e3ff97fb640af3435cc6b72105db

Specify lowpass BW in Hertz, not relative to current sampling rate

code/Decoder/Decoder.h
@@ -158,7 +158,7 @@ private:
 	mutable std::mutex 	afc_mtx_;
 
 	// Low Pass FIR
-	float 				lowpass_bw_ = 0.05;
+	float 				lowpass_bw_Hz_ = 1500;
 	float 				lowpass_trans_ = 0.025;
 	TFIR				lowpass_fir_;
 	mutable std::mutex	lowpass_fir_mtx_;
@@ -216,17 +216,17 @@ void habdec::Decoder<TReal>::init(const float i_sampling_rate)
 
 
 template<typename TReal>
-void 	habdec::Decoder<TReal>::lowpass_bw(float i_lowpass_bw)
+void 	habdec::Decoder<TReal>::lowpass_bw(float i_lowpass_bw_Hz)
 {
 	std::lock_guard<std::mutex>	_lock(lowpass_fir_mtx_);
-	lowpass_bw_ = i_lowpass_bw;
-	lowpass_fir_.LP_BlackmanHarris(lowpass_bw_, lowpass_trans_);
+	lowpass_bw_Hz_ = i_lowpass_bw_Hz;
+	lowpass_fir_.LP_BlackmanHarris(lowpass_bw_Hz_ * getDecimatedSamplingRate(), lowpass_trans_);
 }
 
 template<typename TReal>
 float 	habdec::Decoder<TReal>::lowpass_bw() const
 {
-	return lowpass_bw_;
+	return lowpass_bw_Hz_;
 }
 
 template<typename TReal>
@@ -234,7 +234,7 @@ void 	habdec::Decoder<TReal>::lowpass_trans(float i_lowpass_trans)
 {
 	std::lock_guard<std::mutex>	_lock(lowpass_fir_mtx_);
 	lowpass_trans_ = i_lowpass_trans;
-	lowpass_fir_.LP_BlackmanHarris(lowpass_bw_, lowpass_trans_);
+	lowpass_fir_.LP_BlackmanHarris(lowpass_bw_Hz_ * getDecimatedSamplingRate(), lowpass_trans_);
 }
 
 template<typename TReal>
@@ -516,7 +516,7 @@ void habdec::Decoder<TReal>::process()
 		lock_guard<mutex>	_lock(lowpass_fir_mtx_);
 		lowpass_fir_.setInput (iq_samples_decimated_.data(), samples_to_filter_cnt);
 		lowpass_fir_.setOutput(iq_samples_filtered_.data());
-		lowpass_fir_.LP_BlackmanHarris(lowpass_bw_, lowpass_trans_);
+		lowpass_fir_.LP_BlackmanHarris(lowpass_bw_Hz_ * getDecimatedSamplingRate(), lowpass_trans_);
 		lowpass_fir_();
 		iq_samples_filtered_.samplingRate( getDecimatedSamplingRate() );
 	}

code/pyClient/habdecClient.py
@@ -127,7 +127,7 @@ def SendCommand(i_cmd):
 		set:baud=50				get:baud
 		set:rtty_bits=8			get:rtty_bits
 		set:rtty_stops=2		get:rtty_stops
-		set:lowpass_bw=.1		get:lowpass_bw
+		set:lowpass_bw=1500		get:lowpass_bw
 		set:lowpass_trans=.01	get:lowpass_trans
 		set:biastee=1			get:biastee
 		set:decimation=2		get:decimation

code/pyClient/main_tk.py
@@ -104,10 +104,10 @@ def RedrawSpectrum():
 
 		# LOWPASS FILTER DRAW
 		#
-		_lowpass_bw = hdc.STATE['lowpass_bw'] / (1.0 - .999*zoom)
+		_lowpass_bw_relative = hdc.STATE['lowpass_bw'] / hdc.SPECTRUM.sampling_rate_ / (1.0 - .999*zoom)
 		_lowpass_trans = hdc.STATE['lowpass_trans'] / (1.0 - .999*zoom)
-		_l  = max(0, .5 - .5 * _lowpass_bw)
-		_r  = min(1, .5 + .5 * _lowpass_bw)
+		_l  = max(0, .5 - .5 * _lowpass_bw_relative)
+		_r  = min(1, .5 + .5 * _lowpass_bw_relative)
 		SPECTRUM_CANVAS.create_rectangle(
 						_l*SPECTRUM_CANVAS_RES_X, 0,
 						_r*SPECTRUM_CANVAS_RES_X, SPECTRUM_CANVAS_RES_Y-1,
@@ -304,7 +304,7 @@ def BuildMainWindow():
 		return [widget, var, i_type]
 
 	hdc.UpdateState()
-	print(hdc.STATE)
+
 	global CONTROLS
 	CONTROLS['frequency'] = 	MakeWidget('Float', 'frequency', 	hdc.STATE['frequency'],		partial(ControlCallback, 'frequency') )
 	CONTROLS['gain'] = 			MakeWidget('Int', 	'gain', 		hdc.STATE['gain'], 			partial(ControlCallback, 'gain') )

code/webClient/js/drawing.js
@@ -102,14 +102,14 @@ function DrawPowerSpectrum(i_canvas, i_spectrum)
 	var zoom = G_SPECTRUM_ZOOM;
 
     // LOWPASS FILTER DRAW
-    //
-    var _lowpass_bw = GLOBALS.lowpass_bw / (1.0 - .999*zoom);
+	//
+	var _lowpass_bw_relative = GLOBALS.lowpass_bw / i_spectrum.sampling_rate_ / (1.0 - .999*zoom);
     var _lowpass_trans = GLOBALS.lowpass_trans / (1.0 - .999*zoom);
 	var grd_lowpass = ctx.createLinearGradient(0, 0, i_canvas.width-1, 0);
-	var _l  = Math.max(0, .5 - .5 * _lowpass_bw);
-	var _ll = Math.max(0, .5 - .5 * (_lowpass_bw + _lowpass_trans));
-	var _r  = Math.min(1, .5 + .5 * _lowpass_bw);
-	var _rr = Math.min(1, .5 + .5 * (_lowpass_bw + _lowpass_trans));
+	var _l  = Math.max(0, .5 - .5 * _lowpass_bw_relative);
+	var _ll = Math.max(0, .5 - .5 * (_lowpass_bw_relative + _lowpass_trans));
+	var _r  = Math.min(1, .5 + .5 * _lowpass_bw_relative);
+	var _rr = Math.min(1, .5 + .5 * (_lowpass_bw_relative + _lowpass_trans));
 
 	grd_lowpass.addColorStop(0, "rgba(15,25,50,0)");
 	grd_lowpass.addColorStop(_ll, "rgba(15,25,50,0)");

code/webClient/js/gui.js
@@ -110,7 +110,7 @@ function CreateControls()
 	CreateFloatBoxWithArrows("cnt_gain", "gain",
 						SendCommand,  0, 49, 21, 1, 5);
 	CreateFloatBoxWithArrows("cnt_lowpass_bw", "lowpass_bw",
-						SendCommand,  .0, 1.0, .05, .002, .02);
+						SendCommand,  100, 4000, 1500, 50, 200, 10);
 	CreateFloatBoxWithArrows("cnt_lowpass_trans", "lowpass_trans",
 						SendCommand,  .0025, 0.1, .05, .0025, .01);
 	CreateFloatBoxWithArrows("cnt_baud", "baud",

code/webClient/js/habdec_commands.js
@@ -100,16 +100,11 @@ function ws_onMessage(evt)
 		if(what == "PWR_")
 		{
 			var spectrum = DecodeSpectrum(evt.data, 4);
-			if(GLOBALS.sampling_rate != spectrum.sampling_rate_)
-			{
-				GLOBALS.sampling_rate = spectrum.sampling_rate_;
-				var widget = document.getElementById("frequency");
-				widget.setAttribute("step_big", GLOBALS.sampling_rate / 1e6 / 30);
-				widget.setAttribute("step_small", GLOBALS.sampling_rate / 1e6 / 500);
-			}
+			var widget = document.getElementById("frequency");
+			widget.setAttribute("step_big", spectrum.sampling_rate_ / 1e6 / 30);
+			widget.setAttribute("step_small", spectrum.sampling_rate_ / 1e6 / 500);
 			DrawPowerSpectrum(document.getElementById("powerSpectrumCanvas"), spectrum);
 			RefreshPowerSpectrum_lastReq = 0;
-
 		}
 		else if(what == "DEM_")
 		{

code/websocketServer/main.cpp
@@ -341,8 +341,6 @@ int main(int argc, char** argv)
 	DECODER.baud(GLOBALS::get().baud_);
 	DECODER.rtty_bits(GLOBALS::get().rtty_ascii_bits_);
 	DECODER.rtty_stops(GLOBALS::get().rtty_ascii_stops_);
-	DECODER.lowpass_bw(.05);
-	DECODER.lowpass_trans(.025);
 	DECODER.livePrint( GLOBALS::get().live_print_ );
 	DECODER.dc_remove( GLOBALS::get().dc_remove_ );
 

commit 80428e450161bcff6d236f0bc8ab7f4192751590

Fix incorrect lowPass width calculation. This was critical bug introduced in 1488daf which made habdec useless.

code/Decoder/Decoder.h
@@ -220,7 +220,7 @@ void 	habdec::Decoder<TReal>::lowpass_bw(float i_lowpass_bw_Hz)
 {
 	std::lock_guard<std::mutex>	_lock(lowpass_fir_mtx_);
 	lowpass_bw_Hz_ = i_lowpass_bw_Hz;
-	lowpass_fir_.LP_BlackmanHarris(lowpass_bw_Hz_ * getDecimatedSamplingRate(), lowpass_trans_);
+	lowpass_fir_.LP_BlackmanHarris(lowpass_bw_Hz_ / getDecimatedSamplingRate(), lowpass_trans_);
 }
 
 template<typename TReal>
@@ -234,7 +234,7 @@ void 	habdec::Decoder<TReal>::lowpass_trans(float i_lowpass_trans)
 {
 	std::lock_guard<std::mutex>	_lock(lowpass_fir_mtx_);
 	lowpass_trans_ = i_lowpass_trans;
-	lowpass_fir_.LP_BlackmanHarris(lowpass_bw_Hz_ * getDecimatedSamplingRate(), lowpass_trans_);
+	lowpass_fir_.LP_BlackmanHarris(lowpass_bw_Hz_ / getDecimatedSamplingRate(), lowpass_trans_);
 }
 
 template<typename TReal>
@@ -516,7 +516,7 @@ void habdec::Decoder<TReal>::process()
 		lock_guard<mutex>	_lock(lowpass_fir_mtx_);
 		lowpass_fir_.setInput (iq_samples_decimated_.data(), samples_to_filter_cnt);
 		lowpass_fir_.setOutput(iq_samples_filtered_.data());
-		lowpass_fir_.LP_BlackmanHarris(lowpass_bw_Hz_ * getDecimatedSamplingRate(), lowpass_trans_);
+		lowpass_fir_.LP_BlackmanHarris(lowpass_bw_Hz_ / getDecimatedSamplingRate(), lowpass_trans_);
 		lowpass_fir_();
 		iq_samples_filtered_.samplingRate( getDecimatedSamplingRate() );
 	}